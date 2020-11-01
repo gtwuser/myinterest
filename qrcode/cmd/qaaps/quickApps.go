0ce97e009f8a221b08e6e74145f32db89d4b673d
kapjoshi <kapjoshi@cisco.com>
2020-11-02 02:34:36 +0530

    todo 1

--- qrcode/cmd/qaaps/quickApps.go
+++ qrcode/cmd/qaaps/quickApps.go
@@ -19,6 +19,7 @@ func main() {
 	//if err != nil {
 	//	log.Fatal("Url ", *url, "output could not be read ", resp.StatusCode)
 	//}
+<<<<<<< HEAD
 	//file, _ := ioutil.ReadFile(`./data.xml`)
 	//content := string(file)
 	//re := regexp.MustCompile(`<li.*?>.*?title="(.*?)"`)
@@ -28,4 +29,13 @@ func main() {
 	//}
 	s := []string{"1808596"}
 	fmt.Println(strings.Join(s, `","`))
+
+	// TODO
+	file, _ := ioutil.ReadFile(`./data.xml`)
+	content := string(file)
+	re := regexp.MustCompile(`<li.*?>.*?title="(.*?)"`)
+	allString := re.FindAllStringSubmatch(content, -1)
+	for i, r := range allString {
+		fmt.Println(r[1], i)
+	}
 }
